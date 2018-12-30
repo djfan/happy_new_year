ec7cd7b40718fd6497b639d93aed0381ce66c0e3
Dongjie Fan <djfan@users.noreply.github.com>
2018-12-30 15:08:06 -0500

    Create typeofvar.sh

---
+++ typeofvar.sh
@@ -0,0 +1,17 @@
+
+
+typeofvar () {
+
+    local type_signature=$(declare -p "$1" 2>/dev/null)
+
+    if [[ "$type_signature" =~ "declare --" ]]; then
+        printf "string"
+    elif [[ "$type_signature" =~ "declare -a" ]]; then
+        printf "array"
+    elif [[ "$type_signature" =~ "declare -A" ]]; then
+        printf "map"
+    else
+        printf "none"
+    fi
+
+}
